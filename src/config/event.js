a203da1fece508d8388ae0f256220beda5e23fb2
eclipseeer <eclipseeer@gmail.com>
2021-12-23 10:43:25 +0200

    Add SF event

--- src/config/event.js
+++ src/config/event.js
@@ -1,18 +1,25 @@
-import mLogoText from '../ui/images/miami/miami-text.svg';
-import nyLogoText from '../ui/images/newYork/nyc.svg';
+import miami from '../ui/images/miami/miami.svg';
+import nyc from '../ui/images/newYork/nyc.svg';
+import sf from '../ui/images/sf/sf.svg';
 
 const events = {
   miami: {
     name: 'miami',
     pageTitle: 'NEAR Miami',
-    logoText: mLogoText,
+    logoText: miami,
     mainUrl: 'miami.near.org',
   },
   newYork: {
     name: 'newYork',
     pageTitle: 'NEAR NYC',
-    logoText: nyLogoText,
+    logoText: nyc,
     mainUrl: 'nyc.near.org',
+  },
+  sf: {
+    name: 'sf',
+    pageTitle: 'NEAR SF',
+    logoText: sf,
+    mainUrl: 'sf.near.org',
   },
 };
 
